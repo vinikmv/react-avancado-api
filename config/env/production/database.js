4714b2fc485d88843a11a261cee30986dde9f15f
viniciuskmv <viniciuskmv@gmail.com>
2022-07-19 10:55:09 -0300

    change database config in production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,21 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
+        client: "postgres",
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL", false) ? {
+          require: true,
           rejectUnauthorized: false,
-        },
+        } : false,
       },
-      options: {
-        ssl: true,
-      },
+      options: {},
     },
   },
 });

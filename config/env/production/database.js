6d03c0e4ee3be15586d9978743929af89dfa388d
viniciuskmv <viniciuskmv@gmail.com>
2022-07-19 11:20:45 -0300

    revert dabatase config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,3 +1,6 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
@@ -5,17 +8,18 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: env.bool("DATABASE_SSL", false) ? {
-          require: true,
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
           rejectUnauthorized: false,
-        } : false,
+        },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 });
